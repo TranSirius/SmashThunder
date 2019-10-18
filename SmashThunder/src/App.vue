71e3c339c32b0ab6b59366543d897425db6db2d5
DiscreteTom <discrete_tom@outlook.com>
2019-10-18 23:47:21 +0800

    optimize sign in form ui

--- SmashThunder/src/App.vue
+++ SmashThunder/src/App.vue
@@ -35,54 +35,53 @@
     <b-container style="margin-top:80px">
       <!-- Sign in form -->
       <b-collapse id="signInForm">
-        <b-card>
-          <b-card-body>
-            <b-form>
-              <b-alert variant="danger">{{ formErr }}</b-alert>
-              <b-form-radio-group
-                buttons
-                size="sm"
-                button-variant="outline-primary"
-                :options="signInFormOptions"
-                v-model="formMode"
-                v-on:change="()=>{this.$root.$emit('bv::toggle::collapse', 'password2')}"
-              ></b-form-radio-group>
-              <b-form-group label="Username" label-for="signInUsername">
-                <b-form-input
-                  id="signInUsername"
-                  v-model="username"
-                  placeholder="Enter username"
-                  type="text"
-                  required
-                />
-              </b-form-group>
-              <b-form-group label="Password" label-for="signInPassword">
+        <b-jumbotron header="Welcome!" lead="SmashThunder is a very awsome blog management system.">
+          <b-form>
+            <b-alert variant="danger">{{ formErr }}</b-alert>
+            <b-form-radio-group
+              buttons
+              size="sm"
+              button-variant="outline-primary"
+              :options="signInFormOptions"
+              v-model="formMode"
+              v-on:change="()=>{this.$root.$emit('bv::toggle::collapse', 'password2')}"
+            ></b-form-radio-group>
+            <hr />
+            <b-form-group label="Username" label-for="signInUsername">
+              <b-form-input
+                id="signInUsername"
+                v-model="username"
+                placeholder="Enter username"
+                type="text"
+                required
+              />
+            </b-form-group>
+            <b-form-group label="Password" label-for="signInPassword">
+              <b-form-input
+                type="password"
+                id="signInPassword"
+                placeholder="Password"
+                v-model="password"
+                required
+              />
+            </b-form-group>
+            <b-collapse id="password2">
+              <b-form-group label="Repeat Password" label-for="signInPassword2">
                 <b-form-input
                   type="password"
-                  id="signInPassword"
-                  placeholder="Password"
-                  v-model="password"
+                  id="signInPassword2"
+                  placeholder="Repeat password"
+                  v-model="password2"
                   required
                 />
               </b-form-group>
-              <b-collapse id="password2">
-                <b-form-group label="Repeat Password" label-for="signInPassword2">
-                  <b-form-input
-                    type="password"
-                    id="signInPassword2"
-                    placeholder="Repeat password"
-                    v-model="password2"
-                    required
-                  />
-                </b-form-group>
-              </b-collapse>
-              <b-button variant="primary" @click="submitClicked">
-                <b-spinner label="loading" small v-if="submitting" type="grow"></b-spinner>
-                <span class="sr-only">Loading...</span> Submit
-              </b-button>
-            </b-form>
-          </b-card-body>
-        </b-card>
+            </b-collapse>
+            <b-button variant="primary" @click="submitClicked">
+              <b-spinner label="loading" small v-if="submitting" type="grow"></b-spinner>
+              <span class="sr-only">Loading...</span> Submit
+            </b-button>
+          </b-form>
+        </b-jumbotron>
       </b-collapse>
       <Content></Content>
     </b-container>
