e4a0832a44233d9d207c4d8845aadb9cc792cbaa
DiscreteTom <discrete_tom@outlook.com>
2019-10-20 19:59:12 +0800

    init router

--- SmashThunder/src/components/Content.vue
+++ SmashThunder/src/components/Content.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Content">
     <!-- Friends Activities -->
-    <div v-if="user.loggedIn">
+    <div v-if="$root.$data.user.loggedIn">
       <h2>Friends Activities</h2>
       <hr />
     </div>
@@ -12,9 +12,32 @@
       <b-card
         v-for="num in 10"
         :key="num"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-top
+      >
+        <b-card-title>Blog's Title</b-card-title>
+        <b-card-sub-title class="mb-2">Author</b-card-sub-title>
+        <b-card-text>{{ "Blog's subtitle and other description.".repeat((Math.floor(Math.random() * 10) + 1)) }}</b-card-text>
+        <b-card-text class="small text-muted">
+          Last updated {{ Math.floor(Math.random() * 10) + 1 }} mins
+          ago
+        </b-card-text>
+      </b-card>
+      <b-card
+        v-for="num in 3"
+        :key="num"
         img-src="https://discretetom.github.io/img/me.jpg"
         img-top
       >
+        <b-card-title>Blog's Title</b-card-title>
+        <b-card-sub-title class="mb-2">Author</b-card-sub-title>
+        <b-card-text>{{ "Blog's subtitle and other description.".repeat((Math.floor(Math.random() * 10) + 1)) }}</b-card-text>
+        <b-card-text class="small text-muted">
+          Last updated {{ Math.floor(Math.random() * 10) + 1 }} mins
+          ago
+        </b-card-text>
+      </b-card>
+      <b-card v-for="num in 10" :key="num" img-src="https://picsum.photos/900/250/?image=3" img-top>
         <b-card-title>Blog's Title</b-card-title>
         <b-card-sub-title class="mb-2">Author</b-card-sub-title>
         <b-card-text>{{ "Blog's subtitle and other description.".repeat((Math.floor(Math.random() * 10) + 1)) }}</b-card-text>
@@ -29,9 +52,6 @@
 
 <script>
 export default {
-  name: "Content",
-  props: {
-    user: Object
-  }
+  name: "Content"
 };
 </script>
--- SmashThunder/src/main.js
+++ SmashThunder/src/main.js
@@ -1,13 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import Content from "./components/Content.vue"
+
+const routes = [
+  { path: '/', component: Content },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+
 new Vue({
   render: h => h(App),
+  data: {
+    user: {
+      username: "",
+      password: "",
+      loggedIn: false
+    }
+  },
+  router
 }).$mount('#app')
