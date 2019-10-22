e8a581e043fb2547bfcbf76daa496e0f216f266b
DiscreteTom <discrete_tom@outlook.com>
2019-10-22 10:34:32 +0800

    optimize Header, set text-align=right

--- SmashThunder/src/components/Header.vue
+++ SmashThunder/src/components/Header.vue
@@ -21,17 +21,17 @@
         <b-navbar-nav v-if="user.loggedIn">
           <!-- if logged in -->
           <!-- <b-nav-item :to="user.username">{{ user.username }}</b-nav-item> -->
-          <b-nav-item-dropdown :text="user.username" right>
+          <b-nav-item-dropdown :text="user.username" right class="text-right">
             <b-dropdown-item :to="user.username">Home</b-dropdown-item>
             <b-dropdown-item :to="user.username + '/album'">Album</b-dropdown-item>
             <b-dropdown-item :to="user.username + '/posts'">Posts</b-dropdown-item>
           </b-nav-item-dropdown>
-          <b-nav-item to="#">New</b-nav-item>
-          <b-nav-item to="#" @click="()=>{this.$refs.form.logout()}">Sign out</b-nav-item>
+          <b-nav-item class="text-right" to="#">New</b-nav-item>
+          <b-nav-item class="text-right" to="#" @click="()=>{this.$refs.form.logout()}">Sign out</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav v-else>
           <!-- not logged in -->
-          <b-nav-item v-b-toggle.signInForm>Sign In/Up</b-nav-item>
+          <b-nav-item class="text-right" v-b-toggle.signInForm>Sign In/Up</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
