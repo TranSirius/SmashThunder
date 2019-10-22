0943c5c8df42ef685cf0ababd62f1652301a5416
zijun <transirius@outlook.com>
2019-10-22 10:00:04 +0800

    separate data model from database interface

--- web/web/databases.py
+++ web/web/databases.py
@@ -34,29 +34,7 @@ db_session = scoped_session(session)
 Model = declarative_base(name='Model')
 Model.query = db_session.query_property()
 
-class User(Model):
-    __tablename__ = 'Users'
-
-    ID = Column('ID', Integer, primary_key = True, autoincrement = True)
-    # super_admin, admin, user
-    user_type = Column('user_type', String(50), nullable = False, default = 'user')
-    user_name = Column('user_name', String(200), unique = True, nullable = True)
-    pass_word = Column('pass_word', String(100), nullable = False)
-
-    def __str__(self):
-        return '%s(ID=%r, user_type=%r, user_name=%r, pass_word=%r)' % (
-            self.__class__.__name__,
-            self.ID,
-            self.user_type,
-            self.user_name,
-            self.pass_word
-        )
-
-    def __eq__(self, other):
-        return type(self) is type(other) and self.ID == other.ID
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
+from web.datamodels import User
 
 @click.command('drop-db')
 @with_appcontext
---
+++ web/web/datamodels.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import event
+
+from sqlalchemy.ext.declarative import declarative_base
+from web.databases import db_session
+
+Model = declarative_base(name='BaseDataModel')
+Model.query = db_session.query_property()
+
+class User(Model):
+    __tablename__ = 'Users'
+
+    ID = Column('ID', Integer, primary_key = True, autoincrement = True)
+    # super_admin, admin, user
+    user_type = Column('user_type', String(50), nullable = False, default = 'user')
+    user_name = Column('user_name', String(200), unique = True, nullable = True)
+    pass_word = Column('pass_word', String(100), nullable = False)
+
+    def __str__(self):
+        return '%s(ID=%r, user_type=%r, user_name=%r, pass_word=%r)' % (
+            self.__class__.__name__,
+            self.ID,
+            self.user_type,
+            self.user_name,
+            self.pass_word
+        )
+
+    def __eq__(self, other):
+        return type(self) is type(other) and self.ID == other.ID
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
