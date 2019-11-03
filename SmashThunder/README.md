952c84b18b19b7252495425760ac35b3c166d062
DiscreteTom <discrete_tom@outlook.com>
2019-11-03 16:12:19 +0800

    init Edit

--- SmashThunder/README.md
+++ SmashThunder/README.md
@@ -8,7 +8,7 @@
 - `/:username/album` - User's image management page
 - `/:username/posts` - User's posts management page
 - `/:username/posts/:post` - User's post
-- `/edit` - New post or edit existing post
+- `/:username/edit` - New post or edit existing post
 - `/admin` - Admin homepage
 
 ## Data format
---
+++ SmashThunder/src/components/view/Edit.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <b-form @submit="submit" style="height:500px">
+      <b-form-row>
+        <b-form-group label="Post title" label-for="postTitle">
+          <b-form-input
+            id="postTitle"
+            v-model="form.title"
+            placeholder="Please input the post title"
+          ></b-form-input>
+        </b-form-group>
+      </b-form-row>
+      <!-- <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
+      </b-form-group>-->
+      <b-form-row>
+        <b-col>
+          <b-form-group label="Post Content" label-for="textarea">
+            <b-form-textarea
+              id="textarea"
+              v-model="form.text"
+              placeholder="Enter something..."
+              no-resize
+              rows="20"
+            ></b-form-textarea>
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group label="Realtime Render">
+            <div v-html="resultHTML"></div>
+          </b-form-group>
+        </b-col>
+      </b-form-row>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import showdown from "showdown";
+
+var converter = new showdown.Converter();
+
+export default {
+  name: "Edit",
+  data() {
+    return {
+      form: {
+        text: "# Markdown Here",
+        title: ""
+      }
+    };
+  },
+  methods: {
+    submit() {}
+  },
+  computed: {
+    resultHTML() {
+      return converter.makeHtml(this.form.text);
+    }
+  }
+};
+</script>
