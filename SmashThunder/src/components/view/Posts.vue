390b75e1808801092d3ce23eabeff81d4271a190
DiscreteTom <discrete_tom@outlook.com>
2019-11-12 10:00:21 +0800

    update Post, add tips when no folder

--- SmashThunder/src/components/view/Posts.vue
+++ SmashThunder/src/components/view/Posts.vue
@@ -1,40 +1,43 @@
 <template>
   <div>
-    <div v-for="folder in folders" :key="folder.title">
-      <!-- Folder title and action buttons -->
-      <h2>
-        {{ folder.title }}
-        <b-dropdown split text="Rename" class="mb-1 ml-3" variant="secondary" v-if="editable">
-          <b-dropdown-item variant="danger">Delete</b-dropdown-item>
-        </b-dropdown>
-        <b-button
-          class="mb-1 ml-3"
-          variant="outline-info"
-          :pressed.sync="folder.show"
-        >{{ folder.show ? 'Hide' : 'Show' }}</b-button>
-      </h2>
-      <!-- Posts table -->
-      <b-table
-        :items="folder.posts"
-        :fields="table.fields"
-        hover
-        striped
-        sticky-header
-        sort-icon-left
-        :tbodyTrClass="rowStyle"
-      >
-        <template v-slot:cell(title)="data">
-          <b-link :to="'/'+$route.params.username+'/posts/'+data.item.title">{{ data.item.title }}</b-link>
-        </template>
-        <template v-slot:cell(format)="data">{{ data.item.format=='md'?'Markdown':'Latex' }}</template>
-        <template v-slot:cell(createTime)="data">{{ data.item.createTime | peekDate }}</template>
-        <template v-slot:cell(published)="data">{{ data.item.published ? 'Yes' : 'No' }}</template>
-        <template v-slot:cell(actions)="data">
-          <b-button size="sm" variant="primary">{{ data.item.published ? 'Withdraw' : 'Publish'}}</b-button>
-          <b-button size="sm" class="ml-2" variant="outline-danger">Delete</b-button>
-        </template>
-      </b-table>
+    <div v-if="folders.length">
+      <div v-for="folder in folders" :key="folder.title">
+        <!-- Folder title and action buttons -->
+        <h2>
+          {{ folder.title }}
+          <b-dropdown split text="Rename" class="mb-1 ml-3" variant="secondary" v-if="editable">
+            <b-dropdown-item variant="danger">Delete</b-dropdown-item>
+          </b-dropdown>
+          <b-button
+            class="mb-1 ml-3"
+            variant="outline-info"
+            :pressed.sync="folder.show"
+          >{{ folder.show ? 'Hide' : 'Show' }}</b-button>
+        </h2>
+        <!-- Posts table -->
+        <b-table
+          :items="folder.posts"
+          :fields="table.fields"
+          hover
+          striped
+          sticky-header
+          sort-icon-left
+          :tbodyTrClass="rowStyle"
+        >
+          <template v-slot:cell(title)="data">
+            <b-link :to="'/'+$route.params.username+'/posts/'+data.item.title">{{ data.item.title }}</b-link>
+          </template>
+          <template v-slot:cell(format)="data">{{ data.item.format=='md'?'Markdown':'Latex' }}</template>
+          <template v-slot:cell(createTime)="data">{{ data.item.createTime | peekDate }}</template>
+          <template v-slot:cell(published)="data">{{ data.item.published ? 'Yes' : 'No' }}</template>
+          <template v-slot:cell(actions)="data">
+            <b-button size="sm" variant="primary">{{ data.item.published ? 'Withdraw' : 'Publish'}}</b-button>
+            <b-button size="sm" class="ml-2" variant="outline-danger">Delete</b-button>
+          </template>
+        </b-table>
+      </div>
     </div>
+    <h2 v-else>No folders here.</h2>
   </div>
 </template>
 
