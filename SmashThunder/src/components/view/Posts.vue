76cc44e370052b7a589665a73b9476d8fd5b6c3f
qhdwyx <qhdwyx10@163.com>
2019-11-13 15:39:37 +0800

    add interface to Posts.vue

--- SmashThunder/src/components/view/Posts.vue
+++ SmashThunder/src/components/view/Posts.vue
@@ -5,8 +5,9 @@
         <!-- Folder title and action buttons -->
         <h2>
           {{ folder.title }}
-          <b-dropdown split text="Rename" class="mb-1 ml-3" variant="secondary" v-if="editable">
-            <b-dropdown-item variant="danger">Delete</b-dropdown-item>
+          <b-dropdown split text="Rename" class="mb-1 ml-3" variant="secondary" v-if="editable"
+          @click="showRenameForm(folder.title)">
+            <b-dropdown-item variant="danger" @click="deletePostOrFolder(folder.title)">Delete</b-dropdown-item>
           </b-dropdown>
           <b-button
             class="mb-1 ml-3"
@@ -39,15 +40,24 @@
               variant="primary"
               v-if="editable"
               size="sm"
+              @click="publishPost(folder.title, data.item.title,data.item.published)"
             >
-              <b-dropdown-item variant="secondary">Rename</b-dropdown-item>
-              <b-dropdown-item variant="danger">Delete</b-dropdown-item>
+              <b-dropdown-item variant="secondary" @click="showRenameForm(data.item.title,folder.title)">Rename</b-dropdown-item>
+              <b-dropdown-item variant="danger" @click="deletePostOrFolder(folder.title, data.item.title)">Delete</b-dropdown-item>
             </b-dropdown>
           </template>
         </b-table>
       </div>
     </div>
     <h2 v-else>No folders here.</h2>
+    <!-- rename form -->
+    <ModalInput
+      ref="renameForm"
+      title="Please enter the new name"
+      :ok="rename"
+      v-model="renameForm.newName"
+      placeholder="New name"
+    ></ModalInput>
   </div>
 </template>
 
@@ -72,7 +82,12 @@ export default {
           "actions"
         ]
       },
-      folders: []
+      folders: [],
+      renameForm: {
+        newName: "",
+        oldName: "",
+        folderTitle: ""
+      }
     };
   },
   methods: {
@@ -91,6 +106,166 @@ export default {
         })
         .catch(() => {
           this.toastErr("Get posts error");
+        });
+    },
+    showRenameForm(oldName, folderTitle) {
+      this.renameForm.oldName = this.renameForm.newName = oldName;
+      this.renameForm.folderTitle = folderTitle;
+      this.$refs.renameForm.show();
+    },
+    showRenameErr(s) {
+      this.toastErr("Rename failed", s);
+    },
+    showDelErr(s) {
+      this.toastErr("Delete failed", s);
+    },
+    showPublishErr(s){
+      this.toastErr("Publish failed", s);
+    },
+    rename() {
+      this.$refs.renameForm.hide();
+      if (this.renameForm.oldName == this.renameForm.newName) return;
+      // check file name validity
+      if (!this.checkFileName(this.renameForm.newName)) {
+        this.showRenameErr(this.invalidFileNameHint);
+        return;
+      }
+      var renameFolder = !this.renameForm.postTitle;
+      // check title length
+      if (renameFolder) {
+        if (this.renameForm.newName.length > 50) {
+          this.showRenameErr(
+            "Folder title should be shorter than 50 characters."
+          );
+          return;
+        }
+      } else {
+        if (this.renameForm.newName.length > 50) {
+          this.showRenameErr("Post title should be shorter than 50.");
+          return;
+        }
+      }
+      // construct req params
+      var route = renameFolder ? "/edit/folder/rename" : "/edit/post/rename";
+      var data = renameFolder
+        ? {
+            old: this.renameForm.oldName,
+            new: this.renameForm.newName
+          }
+        : {
+            folder: this.renameForm.folderTitle,
+            title: this.renameForm.oldName,
+            newTitle: this.renameForm.newName
+          };
+      // send req
+      axios.post(route, data).then(
+        res => {
+          if (res.data.status == "ok") {
+            if (renameFolder) {
+              for (let i = 0; i < this.folders.length; ++i) {
+                if (this.folders[i].title == this.renameForm.oldName) {
+                  this.folders[i].title = this.renameForm.newName;
+                  return;
+                }
+              }
+            } else {
+              // rename post
+              for (let i = 0; i < this.folders.length; ++i) {
+                if (this.folders[i].title == this.renameForm.folderTitle) {
+                  for (let j = 0; j < this.folders[i].item.length; ++j) {
+                    if (
+                      this.folders[i].item[j].title == this.renameForm.oldName
+                    ) {
+                      this.folders[i].item[j].title = this.renameForm.newName;
+                      return;
+                    }
+                  }
+                }
+              }
+            }
+          } else {
+            this.showRenameErr(res.data.status);
+          }
+        },
+        () => {
+          this.showRenameErr();
+        }
+      );
+    },
+    deletePostOrFolder(folderTitle, postTitle) {
+      var route;
+      var data;
+      if (postTitle) {
+        route = "/edit/post/delete";
+        data = {
+          folder : folderTitle,
+          title : postTitle
+        };
+      } else {
+        route = "/edit/folder/delete";
+        data = {
+          title : folderTitle
+        };
+      }
+      axios
+        .post(route, data)
+        .then(res => {
+          if (res.data.status == "ok") {
+            if (postTitle) {
+              // delete post
+              for (let i = 0; i < this.folders.length; ++i) {
+                if (this.folders[i].title == folderTitle) {
+                  this.folders[i].item = this.folders[i].item.filter(s => {
+                    return s.title != postTitle;
+                  });
+                  return;
+                }
+              }
+            } else {
+              // delete folder
+              this.folders = this.folders.filter(s => {
+                return s.title != folderTitle;
+              });
+              if (!this.folders.length) this.updatePage();
+            }
+          } else {
+            this.showDelErr(res.data.status);
+          }
+        })
+        .catch(() => {
+          this.showDelErr();
+        });
+    },
+    publishPost(folderTitle, postTitle,isPublished)
+    {
+        var route = "/edit/post/publish";
+        var data = {
+            folder : folderTitle,
+            title : postTitle,
+            publish : !isPublished
+        }
+        axios
+        .post(route, data)
+        .then(res => {
+          if (res.data.status == "ok") {
+            for (let i = 0; i < this.folders.length; ++i) {
+                if (this.folders[i].title == folderTitle) {
+                  for (let j = 0; j < this.folders[i].item.length; ++j) {
+                    if (
+                      this.folders[i].item[j].title == postTitle
+                    ) {
+                      this.folders[i].item[j].published = !isPublished;
+                      return;
+                    }
+                  }
+                }
+              }
+          } else {
+            this.showPublishErr(res.data.status);
+          }
+        })
+        .catch(() => {
+          this.showPublishErr();
         });
     }
   },
