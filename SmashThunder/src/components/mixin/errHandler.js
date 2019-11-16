41a0ca50bb3bee4634181c37d281591ae56ec1ec
DiscreteTom <discrete_tom@outlook.com>
2019-11-16 11:09:27 +0800

    optimize comments in errHandler.js

--- SmashThunder/src/components/mixin/errHandler.js
+++ SmashThunder/src/components/mixin/errHandler.js
@@ -1,11 +1,17 @@
+/**
+ * Because the function `toastErr` need to use `this` so it has to be a mixin.
+ */
 export default {
 	methods: {
-		toastErr(title = 'Error!', msg = 'Internal Server Error!', variant = 'danger', toaster = 'top-right') {
+		/**
+		 * `toastErr(title = 'Error!', msg = 'Internal Server Error!', variant = 'danger', toaster = 'top-right', autoHideDelay = 5000)`
+		 */
+		toastErr(title = 'Error!', msg = 'Internal Server Error!', variant = 'danger', toaster = 'top-right', autoHideDelay = 5000) {
 			this.$bvToast.toast(msg, {
 				title,
 				variant,
 				toaster: 'b-toaster-' + toaster,
-				autoHideDelay: 5000
+				autoHideDelay
 			});
 		}
 	}
