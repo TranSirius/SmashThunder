0d8f6f9a08ce3b47434ff9f189ba29f79f6e7303
DiscreteTom <discrete_tom@outlook.com>
2019-11-16 13:42:29 +0800

    update News.vue to adapt netapi

--- SmashThunder/src/components/view/News.vue
+++ SmashThunder/src/components/view/News.vue
@@ -11,10 +11,11 @@
 
 <script>
 import NewsCards from "../utils/NewsCards.vue";
-import axios from "axios";
+import netapi from "../mixin/netapi";
 
 export default {
   name: "News",
+  mixins: [netapi],
   components: {
     NewsCards
   },
@@ -33,19 +34,15 @@ export default {
   },
   methods: {
     getNews() {
-      axios.post("/get/news", { target: "news" }).then(
-        res => {
-          if (res.data.status == "ok") this.news = res.data.posts;
-        },
-        () => {}
+      this.apiPost(
+        { route: "/get/news", data: { target: "news" } },
+        data => (this.news = data.posts)
       );
     },
     getFriendsActivities() {
-      axios.post("/get/news", { target: "friendsActivities" }).then(
-        res => {
-          if (res.data.status == "ok") this.friendsActivities = res.data.posts;
-        },
-        () => {}
+      this.apiPost(
+        { route: "/get/news", data: { target: "friendsActivities" } },
+        data => (this.friendsActivities = data.posts)
       );
     }
   },
