e0a9f4b3fbd5ded1969c8343f924c9a06e4a4d34
DiscreteTom <discrete_tom@outlook.com>
2019-12-03 19:30:09 +0800

    update PostOverview,  whole card is clickable

--- SmashThunder/src/components/utils/PostOverview.vue
+++ SmashThunder/src/components/utils/PostOverview.vue
@@ -6,22 +6,28 @@
       <b-card
         v-for="post in data"
         :key="post.time"
-        :img-src="post.coverImage?'/data/'+post.author+'/img/'+post.coverAlbum+'/'+post.coverImage:undefined"
-        :img-top="post.coverImage?true:undefined"
+        no-body
+        class="hoverCard"
+        @click="$router.push('/'+post.author+'/posts/'+post.folder+'/'+post.title)"
       >
-        <b-card-title>
-          <b-link :to="'/'+post.author+'/posts/'+post.folder+'/'+post.title">{{ post.title }}</b-link>
-        </b-card-title>
-        <b-card-sub-title class="mb-2">
-          <b-link :to="'/'+post.author">{{ post.author }}</b-link>
-        </b-card-sub-title>
-        <b-card-text>
-          <b-link :to="'/'+post.author+'/posts/'+post.folder+'/'+post.title">{{ post.description }}</b-link>
-        </b-card-text>
-        <b-card-text class="small text-muted">
-          Posted {{ post.time | timeOffset }}
-          ago
-        </b-card-text>
+        <b-card-img
+          v-if="post.coverImage"
+          :src="'/data/'+post.author+'/img/'+post.coverAlbum+'/'+post.coverImage"
+          top
+        ></b-card-img>
+        <b-card-body>
+          <b-card-title>
+            <b-link :to="'/'+post.author+'/posts/'+post.folder+'/'+post.title">{{ post.title }}</b-link>
+          </b-card-title>
+          <b-card-sub-title class="mb-2">
+            <b-link :to="'/'+post.author">{{ post.author }}</b-link>
+          </b-card-sub-title>
+          <b-card-text>{{ post.description }}</b-card-text>
+          <b-card-text class="small text-muted">
+            Posted {{ post.time | timeOffset }}
+            ago
+          </b-card-text>
+        </b-card-body>
       </b-card>
     </b-card-group>
     <div v-else>
@@ -47,5 +53,10 @@ export default {
 <style scoped>
 a {
   color: inherit;
+}
+.hoverCard:hover {
+  cursor: pointer;
+  border-color: #bdbdbd;
+  transition: all 0.2s;
 }
 </style>
