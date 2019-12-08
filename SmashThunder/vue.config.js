06ea76cde0d800d716ae8d01b3effa568c7e3985
DiscreteTom <discrete_tom@outlook.com>
2019-12-08 18:56:18 +0800

    update vue.config.js to fix CDN loading problem

--- SmashThunder/vue.config.js
+++ SmashThunder/vue.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
+const config = require("./webpack.config.js")
 
 module.exports = {
   outputDir: "/share/SmashThunder",
-  assetsDir: "static"
+  assetsDir: "static",
+  configureWebpack: config, // https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
 }
