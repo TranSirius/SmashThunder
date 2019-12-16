0625d5379f06874eb840dd83505d2388bd7b2bcf
DiscreteTom <discrete_tom@outlook.com>
2019-12-16 19:17:41 +0800

    Update Header.vue

--- SmashThunder/src/components/Header.vue
+++ SmashThunder/src/components/Header.vue
@@ -12,16 +12,16 @@
       <b-collapse is-nav id="nav-collapse">
         <b-navbar-nav class="ml-auto mt-1 mb-auto">
           <!-- Search bar -->
-          <b-nav-form class="form-inline ml-auto" autocomplete="off" @submit.prevent="onSubmit">
+          <b-nav-form class="form-inline ml-auto" autocomplete="off" @submit.prevent="search">
             <b-input-group>
-              <b-form-input size="sm" placeholder="User post or photo" autofocus="autofocus" ref="searchbox"/>
+              <b-form-input
+                size="sm"
+                placeholder="User post or photo"
+                autofocus="autofocus"
+                v-model="searchContent"
+              />
               <b-input-group-append>
-                <b-button variant="outline-light" size="sm"   @click="search()">Search</b-button>
-                <!-- <b-dropdown right class="mb-1" variant="outline-light" size="sm">
-                  <b-dropdown-item class="text-right" @click="searchusers()">Search Users</b-dropdown-item>
-                  <b-dropdown-item class="text-right" @click="searchposts()">Search Posts</b-dropdown-item>
-                  <b-dropdown-item class="text-right" @click="searchphotos()">Search Photos</b-dropdown-item>
-                </b-dropdown> -->
+                <b-button variant="outline-light" size="sm" type="submit">Search</b-button>
               </b-input-group-append>
             </b-input-group>
           </b-nav-form>
@@ -32,7 +32,10 @@
             <b-dropdown-item class="text-right" :to="'/'+$root.$data.user.username">Home</b-dropdown-item>
             <b-dropdown-item class="text-right" :to="'/'+$root.$data.user.username + '/album'">Album</b-dropdown-item>
             <b-dropdown-item class="text-right" :to="'/'+$root.$data.user.username + '/posts'">Posts</b-dropdown-item>
-            <b-dropdown-item class="text-right" :to="'/'+$root.$data.user.username + '/manage'">Manage</b-dropdown-item>
+            <b-dropdown-item
+              class="text-right"
+              :to="'/'+$root.$data.user.username + '/manage'"
+            >Manage</b-dropdown-item>
           </b-nav-item-dropdown>
           <b-nav-item class="text-right" :to="'/'+$root.$data.user.username+'/edit'">New</b-nav-item>
           <b-nav-item class="text-right" to="#" @click="()=>{this.$refs.form.logout()}">Sign out</b-nav-item>
@@ -48,34 +51,19 @@
 
 <script>
 import SignForm from "./utils/SignForm.vue";
-import netapi from "./mixin/netapi";
-import timeFilter from "./mixin/timeFilter";
-import strCheck from "./mixin/strCheck";
-import arrayCheck from "./mixin/arrayCheck";
-import downloadUtils from "./mixin/downloadUtils";
+
 export default {
   name: "Navbar",
-  mixins: [timeFilter, strCheck, netapi, arrayCheck, downloadUtils],
   components: {
     SignForm
   },
-  methods: {
-    search(){
-      this.$router.push('/search?keyword=' + this.$refs.searchbox.$refs.input.value);
-    },
-    keyupEnter(){
-      document.onkeydown = e =>{
-        if (e.keyCode === 13) {
-          this.search();
-        }
-      }
-    },
-    onSubmit(){
-      return false;
-    }
+  data() {
+    return { searchContent: "" };
   },
-  created(){
-       this.keyupEnter()
+  methods: {
+    search() {
+      this.$router.push("/search?keyword=" + this.searchContent);
+    }
   }
 };
 </script>
