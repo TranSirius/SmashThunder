fe5ad27f7956e53219839171d4a9a432c7fe12b5
DiscreteTom <discrete_tom@outlook.com>
2019-12-17 10:33:58 +0800

    update 404, will auto jump to index

--- SmashThunder/src/components/view/Page404.vue
+++ SmashThunder/src/components/view/Page404.vue
@@ -1,9 +1,33 @@
 <template>
-	<h1>404 Not Found</h1>
+  <div>
+    <h1>404 Not Found</h1>
+    <h2>Will jump to index page in {{ countDown }}s.</h2>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "Page404",
-}
+  name: "Page404",
+  data() {
+    return {
+      countDown: 3
+    };
+  },
+  methods: {
+    enter() {
+      toIndex();
+    },
+    toIndex() {
+      if (this.countDown) {
+        this.countDown--;
+        setTimeout(this.toIndex);
+      } else {
+        this.$router.push("/");
+      }
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    next(v => v.enter());
+  }
+};
 </script>
